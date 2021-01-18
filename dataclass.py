9f3fb3b3371a04cdec54343ae1f24af4a802f01c
Marc Paterno <paterno@fnal.gov>
2021-01-18 17:25:52 -0600

    Add timing output to dataclass demo

--- dataclass.py
+++ dataclass.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import numpy as np
+from time import time as now
 
 
 @dataclass(frozen=True)
@@ -11,6 +12,8 @@ class Position:
 
 size = 100 * 1000
 
-p1 = Position(x=np.ones(size), y=np.ones(size))
+if __name__ == "__main__":
+    from timeit import timeit
 
-p2 = {"x": np.ones(size), "y": np.ones(size)}
+    print(timeit("p1 = Position(x=np.ones(size), y=np.ones(size))", globals = globals()))
+    print(timeit("p2 = {'x': np.ones(size), 'y': np.ones(size)}", globals=globals()))
