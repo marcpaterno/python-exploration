8bb22bd8c08776f67e367c974463b1ead6d1af66
Marc Paterno <paterno@fnal.gov>
2021-01-18 13:06:56 -0600

    Reformat with black

--- dataclass.py
+++ dataclass.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 import numpy as np
 
-@dataclass(frozen = True)
+
+@dataclass(frozen=True)
 class Position:
-    __slots__ = ['x', 'y']
+    __slots__ = ["x", "y"]
     x: np.ndarray
     y: np.ndarray
+
 
 size = 100 * 1000
 
 p1 = Position(x=np.ones(size), y=np.ones(size))
 
-p2 = {'x': np.ones(size), 'y': np.ones(size) }
+p2 = {"x": np.ones(size), "y": np.ones(size)}
