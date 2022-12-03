db83d5f878970177fbf3a6341df99f1b70dce7cc
Marc Paterno <paterno@fnal.gov>
2022-12-03 16:44:06 -0600

    Tracked metaclass preliminary, not yet working

--- meta/test_tracked.py
+++ meta/test_tracked.py
@@ -2,9 +2,10 @@ import pytest
 from tracked import Tracked
 
 
-class Derived(Tracked):
+class Child(Tracked):
     def __init__(self, a, bb, xyz):
         """The names 'a' and 'bb' will be in short_names, but not xyz"""
+        super().__init__()
         self.a = a
         self.bb = bb
         self.xyz = xyz
@@ -17,8 +18,14 @@ class Derived(Tracked):
         self.z = True
 
 
-def test_1():
-    d = Derived(1, "cow", 1.5)
+class Grandchild(Child):
+    def _init__(self, a, bb, xyz, cc):
+        super.__init__(a, bb, xyz)
+        self.cc = cc
+
+
+def test_child():
+    d = Child(1, "cow", 1.5)
     # Make sure the instance variables we initialized are as expected...
     assert d.a == 1
     assert d.bb == "cow"
@@ -34,3 +41,23 @@ def test_1():
 
     # Verify that short_names has not grown a new value.
     assert d.short_names == ["a", "bb"]
+
+
+def test_grandchild():
+    g = Grandchild(1, "cow", 1.5, [1])
+    # Make sure the instance variables we initialized are as expected...
+    assert g.a == 1
+    assert g.bb == "cow"
+    assert g.xyz == 1.5
+    assert g.cc == [1]
+
+    # Make sure short_names contains all the right values and no wrong values...
+    assert g.short_names == ["a", "bb", "cc"]
+
+    # Call the function that adds a new instance variable, make sure it is as
+    # expected...
+    g.grow_wart()
+    assert g.z
+
+    # Verify that short_names has not grown a new value.
+    assert g.short_names == ["a", "bb", "cc"]
